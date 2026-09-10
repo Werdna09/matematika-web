fe6fa96f4cdf90844eba5736b86b9f1b33fc25d3
Werdna09 <f159elix@gmail.com>
2026-09-10 22:41:38 +0200

    Add search filters

--- materials/views.py
+++ materials/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import get_object_or_404, render
 from django.db.models import Q
-
+from django.shortcuts import get_object_or_404, render
 
 from .models import Grade, Material, MaterialType, Topic
 
@@ -77,29 +76,54 @@ def material_detail(request, slug):
 def search(request):
     query = request.GET.get("q", "").strip()
 
-    materials = Material.objects.none()
+    selected_grade = request.GET.get("grade", "")
+    selected_topic = request.GET.get("topic", "")
+    selected_type = request.GET.get("type", "")
+
+    materials = (
+        Material.objects.filter(
+            status=Material.Status.PUBLISHED,
+        )
+        .select_related(
+            "topic",
+            "material_type",
+        )
+        .prefetch_related(
+            "grades",
+            "tags",
+        )
+    )
 
     if query:
-        materials = (
-            Material.objects.filter(
-                Q(title__icontains=query)
-                | Q(description__icontains=query)
-                | Q(topic__name__icontains=query)
-                | Q(material_type__name__icontains=query)
-                | Q(tags__name__icontains=query)
-                | Q(grades__name__icontains=query),
-                status=Material.Status.PUBLISHED,
-            )
-            .select_related(
-                "topic",
-                "material_type",
-            )
-            .prefetch_related(
-                "grades",
-                "tags",
-            )
-            .distinct()
+        materials = materials.filter(
+            Q(title__icontains=query)
+            | Q(description__icontains=query)
+            | Q(topic__name__icontains=query)
+            | Q(material_type__name__icontains=query)
+            | Q(tags__name__icontains=query)
+            | Q(grades__name__icontains=query)
         )
+
+    if selected_grade:
+        materials = materials.filter(
+            grades__id=selected_grade
+        )
+
+    if selected_topic:
+        materials = materials.filter(
+            topic__id=selected_topic
+        )
+
+    if selected_type:
+        materials = materials.filter(
+            material_type__id=selected_type
+        )
+
+    materials = materials.distinct()
+
+    grades = Grade.objects.all()
+    topics = Topic.objects.all()
+    material_types = MaterialType.objects.all()
 
     return render(
         request,
@@ -107,5 +131,27 @@ def search(request):
         {
             "query": query,
             "materials": materials,
+
+            "grades": grades,
+            "topics": topics,
+            "material_types": material_types,
+
+            "selected_grade_id": (
+                int(selected_grade)
+                if selected_grade.isdigit()
+                else None
+            ),
+
+            "selected_topic_id": (
+                int(selected_topic)
+                if selected_topic.isdigit()
+                else None
+            ),
+
+            "selected_type_id": (
+                int(selected_type)
+                if selected_type.isdigit()
+                else None
+            ),
         },
     )
